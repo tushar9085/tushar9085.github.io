c7890f0f636aa2f8b8263413b63204a4eb9f648c
tushar9085 <tushardas9085@gmail.com>
2024-10-12 23:19:31 +0600

    research, project, ref

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu-button').addEventListener('click', function() {
+    var mobileNavbar = document.getElementById('mobile-navbar');
+    mobileNavbar.classList.toggle('active'); // Toggle the "active" class
+});
